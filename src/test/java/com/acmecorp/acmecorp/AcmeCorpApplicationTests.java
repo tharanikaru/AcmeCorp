78c4683e2cc48159c8a42b9362b020cb9a94f41e
tharanikaru <153253126+tharanikaru@users.noreply.github.com>
2023-12-10 12:35:05 +0530

    create initial project (#3)
    Co-authored-by: naveen9513 <naveenavishkauom@gmail.com>

---
+++ src/test/java/com/acmecorp/acmecorp/AcmeCorpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acmecorp.acmecorp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeCorpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
